1d2223388b3dd7665c05f079f06b20ec481ba2d7
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-18 11:54:52 +0300

    add profile class model

--- gram/models.py
+++ gram/models.py
@@ -31,3 +31,35 @@ class Image(models.Model):
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+    bio= models.CharField(max_length=100)
+    prof_pic= CloudinaryField('image')
+    following= models.ManyToManyField(User, related_name='follower', blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+    def save_profile(self):
+        self.save()
+    
+    def delete_profile(self):
+        self.delete()
+
+    def profiles_posts(self):
+        return self.image_set.all()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+    class Meta:
+        ordering=('-created',)
+        
+      

8b5e358405bd01ad0853491ad4c855b2d04cb349
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-18 11:58:19 +0300

    add follow class model

--- gram/models.py
+++ gram/models.py
@@ -5,33 +5,6 @@ from django.urls import reverse
 
 
 # Create your models here.
-class Image(models.Model):
-    image= CloudinaryField('image')
-    author= models.ForeignKey(Profile, on_delete=models.CASCADE)
-    name= models.CharField(max_length=30)
-    caption= models.TextField()
-    comments = models.CharField(max_length=30,blank=True)
-    likes=models.ManyToManyField(User, related_name='blog_posts')
-    pub_date=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-    def total_likes(self):
-        return self.likes.count()
-    
-    def save_image(self):
-        self.save()
-    
-    def delete_image(self):
-        self.delete()
-    
-    def update_image(self):
-        self._do_update()
-    
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk':self.pk})
-
 
 class Profile(models.Model):
     user=models.OneToOneField(User,null=True, on_delete=models.CASCADE)
@@ -64,6 +37,38 @@ class Profile(models.Model):
         
 
 
+
+class Image(models.Model):
+    image= CloudinaryField('image')
+    author= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name= models.CharField(max_length=30)
+    caption= models.TextField()
+    comments = models.CharField(max_length=30,blank=True)
+    likes=models.ManyToManyField(User, related_name='blog_posts')
+    pub_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def total_likes(self):
+        return self.likes.count()
+    
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
+        self._do_update()
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
+
+
+
+
+
 class Comment(models.Model):
     user_id= models.ForeignKey(Profile, on_delete=models.CASCADE)
     comment=models.CharField(max_length=200)
@@ -71,3 +76,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.user_id} :{self.comment}'
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'            
